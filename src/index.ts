79b4db279fd1b63aade343084cba365d361bf9dd
gitFaisal <faisalmalik34@gmail.com>
2022-08-08 13:06:00 -0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const main = async () => {
+  const app: Express = express();
+
+  app.get("/", (_req, res) => res.send("hello world"));
+
+  const PORT = process.env.PORT || 8000;
+
+  app.listen(PORT, () =>
+    console.log(`The server has started on port: ${PORT}`)
+  );
+};
+
+main().catch((err) => console.log(err));
